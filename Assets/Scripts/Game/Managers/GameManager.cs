da8f2ec058fcbbb7200f60b5200be9e9872f2e01
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-03-16 17:17:30 -0300

    created singleton GameManager

---
+++ Assets/Scripts/Game/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Game.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Singleton
+        public static GameManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if  (Instance == null)
+                Instance = this;
+            else Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+        #endregion
+    }
+}
