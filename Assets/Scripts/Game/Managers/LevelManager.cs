3c985c8b4d853cc07d3233005b80417444d803be
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-03-16 17:55:02 -0300

    created a level manager to the game

--- Assets/Scripts/Game/Managers/LevelManager.cs
+++ Assets/Scripts/Game/Managers/LevelManager.cs
@@ -1,12 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 namespace Game.Managers
 {
     public class LevelManager : MonoBehaviour
     {
+        [SerializeField]
+        private TMP_Text _timerText;
+        private float _timer = 60.00f;
+
         private void Awake()
         {
             GameManager.LevelManager = this;
+        }
+
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+            _timerText.text = _timer.ToString().Substring(0, _timer.ToString().IndexOf('.') + 3);
         }
     }
 }
