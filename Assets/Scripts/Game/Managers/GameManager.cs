d19688ab1623b00f5a474eae652425ec51395574
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-03-17 12:21:13 -0300

    updated GameManager

--- Assets/Scripts/Game/Managers/GameManager.cs
+++ Assets/Scripts/Game/Managers/GameManager.cs
@@ -1,3 +1,5 @@
+using AI.State;
+using Game.States;
 using UnityEngine;
 
 namespace Game.Managers
@@ -14,10 +16,37 @@ namespace Game.Managers
             else Destroy(gameObject);
             DontDestroyOnLoad(gameObject);
         }
+        
         #endregion
         
         #region Managers
         public static LevelManager LevelManager { get; set; }
+        #endregion
+        
+        #region MonoBehaviour
+
+        private void Start()
+        {
+            SetupStatemachine();
+        }
+        #endregion
+        
+        #region State
+        public CountingState CountingState { get; private set; }
+        public GameoverState GameoverState { get; private set; }
+        public GameplayState GameplayState { get; private set; }
+        public PausingState PausingState { get; private set; }
+        public StateMachine StateMachine { get; private set; }
+        private void SetupStatemachine()
+        {
+            CountingState = new CountingState();
+            GameoverState = new GameoverState();
+            GameplayState = new GameplayState();
+            PausingState = new PausingState();
+
+            StateMachine = GetComponent<StateMachine>();
+            StateMachine.Initialize(CountingState);
+        }
         #endregion
     }
 }
