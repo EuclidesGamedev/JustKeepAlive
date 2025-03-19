d9e7dd9a02ea0ab02c4a931044029d2620e8d4b4
EuclidesGamedev <193931530+EuclidesGamedev@users.noreply.github.com>
2025-03-19 13:32:47 -0300

    updated Water projectile

---
+++ Assets/Scripts/Objects/Water.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using Utils;
+
+namespace Objects
+{
+    public class Water : PooledObject
+    {
+        private void Update()
+        {
+            if (transform.position.y < -5.25)
+                __objectPool.Release(this);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                collision.gameObject.SetActive(false);
+                __objectPool.Release(this);
+            }
+        }
+    }
+}
